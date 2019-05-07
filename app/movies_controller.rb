65d7fa507f3e6d57105b417915e53b49f01b1732
Richard Yance <yance0022@gmail.com>
2019-05-07 18:03:00 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -6,7 +6,7 @@
 # end                              # end
 
 def can_be_instantiated_and_then_saved
-  movie = movie.new
+  movie = Movie.new
   movie.title = "This is a title."
   movie.save
 end
@@ -35,19 +35,19 @@ def can_be_created_in_a_block(title:"Home Alone", release_date:1990)
 end
 
 def can_get_the_first_item_in_the_database
-  __
+  Movie.first
 end
 
 def can_get_the_last_item_in_the_database
-  __
+  Movie.last
 end
 
 def can_get_size_of_the_database
-  __
+  Movie.count
 end
 
 def can_find_the_first_item_from_the_database_using_id
-  __
+  Movie.find(1)
 end
 
 def can_find_by_multiple_attributes
@@ -55,7 +55,7 @@ def can_find_by_multiple_attributes
   # title == "Title"
   # release_date == 2000
   # director == "Me"
-  __
+  Movie.find_by(title: 'Title', release_date: 2000, director: "Me")
 end
 
 def can_find_using_where_clause_and_be_sorted
